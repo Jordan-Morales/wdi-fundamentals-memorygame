c3e1f28b4dbcc59fb518159ba87c791e5410508d
Jordan Morales <jordan.morales@gmail.com>
2019-08-05 13:44:20 -0700

    memory logic add

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,17 +2,19 @@ console.log("Up and running!");
 const cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!');
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log('You found a match!');
 	}
 	else {
-		alert('Sorry, try again.');
+		console.log('Sorry, try again.');
 	}
+}
+
+function flipCard(cardId) {
+
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+
+}
